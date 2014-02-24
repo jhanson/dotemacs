98589a602927253d5c4d2ce88596fcb4a758b599
Joseph Hanson <jhanson@zenoss.com>
2014-02-24 15:54:11 -0600

    add shortcuts for compiling serviced

--- go-config.el
+++ go-config.el
@@ -6,8 +6,8 @@
 
 (defun flymake-go-init ()
   (list flymake-go-executable
-		(list "fmt"
-			  (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))))
+        (list "fmt"
+              (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))))
 
 (push '(".+\\.go$" flymake-go-init) flymake-allowed-file-name-masks)
 
@@ -18,7 +18,7 @@
   "Tabify current buffer"
   (interactive)
   (if (not (string= major-mode "makefile-gmake-mode"))
-	  (tabify (point-min) (point-max))))
+      (tabify (point-min) (point-max))))
 
 (defun gomodes-hooks ()
   "Hooks for programming modes"
@@ -32,14 +32,35 @@
 (defun gomodes-write-hooks ()
   "Hooks which run on file write for go mode files"
   (if (string= major-mode "go-mode")
-	  (prog1 nil
-		(set-buffer-file-coding-system 'utf-8-unix)
-		(tabify-buffer)
-		(delete-trailing-whitespace)
-		(gofmt))))
+      (prog1 nil
+        (set-buffer-file-coding-system 'utf-8-unix)
+        (tabify-buffer)
+        (delete-trailing-whitespace)
+        (gofmt))))
 
 (add-hook 'go-mode-hook 'gomodes-hooks)
 
-
-
 (define-key global-map "\M-g" 'goto-line)
+
+(defun build-control-plane()
+  "Runs make on the control plane"
+  (interactive)
+  (compile "make -C /home/jhanson/src/go/src/github.com/zenoss/serviced/ -k"))
+
+(global-set-key (kbd "<f6>") 'build-control-plane)
+
+
+(defun test-control-plane()
+  "Runs make test on the control plan in a separate buffer also moves the cursor to that buffer"
+  (interactive)
+  (delete-other-windows)
+  (compile "make -C /home/jhanson/src/go/src/github.com/zenoss/serviced/ -k test")
+  (other-window 1)
+  (goto-char (point-max)))
+  
+(global-set-key (kbd "<f7>") 'test-control-plane)
+
+;;; TODO shortcut for make and make test
+;; go test this specific method
+;; go vet the current buffer
+;; command to run serviced in a gud buffer with a break point at the current line
