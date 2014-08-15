6142331cb996a73f0160276664e80b43b49fe573
Joseph Hanson <jhanson@zenoss.com>
2014-08-15 09:35:24 -0400

    removed large file warning

--- linux-config.el
+++ linux-config.el
@@ -1,5 +1,16 @@
 ;; ubuntu (vmware) specific settings
 (menu-bar-mode -1)
-
-;; zenoss specific mysql (i don't use it for anything else)
+(setq large-file-warning-threshold nil)
+;; zenoss specific mysql 
 (setq sql-mysql-program "/usr/bin/mysql" )
+
+(setenv "ZENHOME"
+        "/opt/zenoss"
+        )
+
+(setenv "PYTHONPATH"
+        "/opt/zenoss/lib/python:/home/zenoss/4.2/core/Products"
+        )
+(setenv "INSTANCE_HOME"
+        "/opt/zenoss"
+        )
