0dbe319580269a3afa70fcf9cc057e9918f98b1b
Joseph Hanson <jhanson@zenoss.com>
2014-10-16 14:46:52 -0500

    fixing commit

--- mac-config.el
+++ mac-config.el
@@ -22,6 +22,8 @@
                         load-path))
 
 (setenv "PS1" "\\u:\\w$ ")
+;; mac theme
+(load-theme 'wombat)
 ;; mac only font
 (set-default-font "-apple-Menlo-bold-normal-normal-*-*-*-*-*-m-0-iso10646-1")
 
@@ -32,49 +34,14 @@
 (global-set-key (kbd "<end>") 'move-end-of-line)
 (global-set-key (kbd "<home>") 'back-to-indentation)
 
-;; control Itunes with the f6 key
 (load-file "~/emacs/pianobar.el")
 (setq pianobar-program-command "/opt/local/bin/pianobar")
 (setq pianobar-username "jrh0090@gmail.com")
 
 
-;; set the color theme
-(require 'color-theme)
-(load-file "~/emacs/color-theme-blackboard.el")
-(color-theme-sanityinc-solarized-dark)
-;; drag a file to emacs to open it
 (define-key global-map [ns-drag-file] 'ns-find-file)
 
 (set-frame-parameter (selected-frame) 'alpha '(99 85))
-(add-to-list 'exec-path "/usr/local/mysql/bin")
-(add-to-list 'exec-path "/opt/local/sbin")
+
 (add-to-list 'exec-path (getenv "PATH"))
 (setq ns-pop-up-frames nil) ;; keep OSX from opening more windows
-
-;; alias the fullscreen command (I never use hide)
-
-(defun ns-small-window ()
-  "Make the window smaller"
-  (interactive)
-  (set-frame-height (selected-frame) 50)
-  (set-frame-width (selected-frame) 180))
-
-(defun ns-maximize-window ()
-  "Make the window smaller"
-  (interactive)
-  (set-frame-position (selected-frame) 0 0)
-  (set-frame-height (selected-frame) 1000)
-  (set-frame-width (selected-frame) 1000))
-
-(define-key global-map [?\s-h] 'ns-maximize-window)
-
-
-;; zenoss specific mysql (i don't use it for anything else)
-(setq sql-mysql-program "/usr/local/mysql/bin/mysql" )
-
-(setq sql-postgres-program "/Library/PostgreSQL/9.1/bin/psql")
-
-;; short cut aliases
-(add-hook 'shell-mode-hook '(lambda ()
-                              (local-set-key "\M-ss" 'dirs)
-                              ))
